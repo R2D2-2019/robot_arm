621814d1382275664ba2570755df2e1e64b3cd80
MarcDirven <marc.dirven@student.hu.nl>
2019-04-05 16:24:22 +0200

    formatted according to .clang, test cases are now in the 'test'folder

--- code/src/coordinate_3D_c.cpp
+++ code/src/coordinate_3D_c.cpp
@@ -5,19 +5,19 @@ namespace r2d2::robot_arm {
         return x == rhs.x && y == rhs.y && z == rhs.z;
     }
 
-    coordinate_3D_c::coordinate_3D_c():
-        coordinate_3D_c(0, 0, 0)
-    {}
+    coordinate_3D_c::coordinate_3D_c() : coordinate_3D_c(0, 0, 0) {
+    }
 
-    coordinate_3D_c::coordinate_3D_c(const int &x, const int &y, const int &z):
-        x(x), y(y), z(z)
-    {}
+    coordinate_3D_c::coordinate_3D_c(const int &x, const int &y, const int &z)
+        : x(x), y(y), z(z) {
+    }
 
-    coordinate_3D_c::coordinate_3D_c(const int &all_values):
-        coordinate_3D_c(all_values, all_values, all_values)
-    {}
+    coordinate_3D_c::coordinate_3D_c(const int &all_values)
+        : coordinate_3D_c(all_values, all_values, all_values) {
+    }
 
-    coordinate_3D_c coordinate_3D_c::operator+(const coordinate_3D_c &rhs) const {
+    coordinate_3D_c coordinate_3D_c::
+    operator+(const coordinate_3D_c &rhs) const {
         return coordinate_3D_c(x + rhs.x, y + rhs.y, z + rhs.z);
     }
 
@@ -25,20 +25,21 @@ namespace r2d2::robot_arm {
         return *this + coordinate_3D_c(rhs);
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator+=(const coordinate_3D_c &rhs) {
+    coordinate_3D_c &coordinate_3D_c::operator+=(const coordinate_3D_c &rhs) {
         *this = *this + rhs;
         return *this;
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator+=(const int &rhs) {
+    coordinate_3D_c &coordinate_3D_c::operator+=(const int &rhs) {
         return *this += coordinate_3D_c(rhs);
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator++(int) {
+    coordinate_3D_c &coordinate_3D_c::operator++(int) {
         return *this += 1;
     }
 
-    coordinate_3D_c coordinate_3D_c::operator-(const coordinate_3D_c &rhs) const {
+    coordinate_3D_c coordinate_3D_c::
+    operator-(const coordinate_3D_c &rhs) const {
         return coordinate_3D_c(x - rhs.x, y - rhs.y, z - rhs.z);
     }
 
@@ -46,36 +47,36 @@ namespace r2d2::robot_arm {
         return *this - coordinate_3D_c(rhs);
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator-=(const coordinate_3D_c &rhs) {
+    coordinate_3D_c &coordinate_3D_c::operator-=(const coordinate_3D_c &rhs) {
         *this = *this - rhs;
         return *this;
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator-=(const int &rhs) {
+    coordinate_3D_c &coordinate_3D_c::operator-=(const int &rhs) {
         return *this -= coordinate_3D_c(rhs);
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator--(int) {
+    coordinate_3D_c &coordinate_3D_c::operator--(int) {
         return *this -= 1;
     }
 
-    coordinate_3D_c coordinate_3D_c::operator/(const coordinate_3D_c &rhs) const {
-        return coordinate_3D_c(static_cast<int>(x / rhs.x), 
-            static_cast<int>(y / rhs.y),
-            static_cast<int>(z / rhs.z));
+    coordinate_3D_c coordinate_3D_c::
+    operator/(const coordinate_3D_c &rhs) const {
+        return coordinate_3D_c(static_cast<int>(x / rhs.x),
+                               static_cast<int>(y / rhs.y),
+                               static_cast<int>(z / rhs.z));
     }
 
     coordinate_3D_c coordinate_3D_c::operator/(const int &rhs) const {
         return *this / coordinate_3D_c(rhs);
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator/=(const coordinate_3D_c &rhs) {
+    coordinate_3D_c &coordinate_3D_c::operator/=(const coordinate_3D_c &rhs) {
         *this = *this / rhs;
         return *this;
     }
 
-    coordinate_3D_c& coordinate_3D_c::operator/=(const int &rhs) {
+    coordinate_3D_c &coordinate_3D_c::operator/=(const int &rhs) {
         return *this /= coordinate_3D_c(rhs);
     }
-}
-
+} // namespace r2d2::robot_arm
--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,109 @@
-#include "ostream"
+#include "coordinate_3D_c.hpp"
+#include <iostream>
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+#include <iostream>
+
+TEST_CASE("Default constructor") {
+    r2d2::robot_arm::coordinate_3D_c c;
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(0, 0, 0));
+}
+
+TEST_CASE("Constructor, one value") {
+    r2d2::robot_arm::coordinate_3D_c c(5);
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(5, 5, 5));
+}
+
+TEST_CASE("Constructor, three values") {
+    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(1, 2, 3));
+}
+
+TEST_CASE("Operator + with coordinate") {
+    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    r2d2::robot_arm::coordinate_3D_c result =
+        c + r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(2, 4, 6));
+}
+
+TEST_CASE("Operator + with int") {
+    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    r2d2::robot_arm::coordinate_3D_c result = c + 1;
+    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(2, 3, 4));
+}
+
+TEST_CASE("Operator += with coordinate") {
+    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    c += r2d2::robot_arm::coordinate_3D_c(3, 4, 5);
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(4, 6, 8));
+}
+
+TEST_CASE("Operator += with int") {
+    r2d2::robot_arm::coordinate_3D_c c(4, 5, 6);
+    c += 3;
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(7, 8, 9));
+}
+
+TEST_CASE("Operator++") {
+    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    c++;
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(2, 3, 4));
+}
+
+TEST_CASE("Operator- with coordinate") {
+    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    r2d2::robot_arm::coordinate_3D_c result =
+        c - r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(0, 0, 0));
+}
+
+TEST_CASE("Operator- with int") {
+    r2d2::robot_arm::coordinate_3D_c c(5, 2, 3);
+    r2d2::robot_arm::coordinate_3D_c result = c - 2;
+    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(3, 0, 1));
+}
+
+TEST_CASE("Operator-= with coordinate") {
+    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
+    c -= r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(4, 2, 0));
+}
+
+TEST_CASE("Operator-= with int") {
+    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
+    c -= 3;
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(2, 1, 0));
+}
+
+TEST_CASE("Operator--") {
+    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
+    c--;
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(4, 3, 2));
+}
+
+TEST_CASE("Operator/ with coordinate") {
+    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
+    r2d2::robot_arm::coordinate_3D_c result =
+        c / r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(5, 2, 1));
+}
+
+TEST_CASE("Operator/ with int") {
+    r2d2::robot_arm::coordinate_3D_c c(4, 10, 20);
+    r2d2::robot_arm::coordinate_3D_c result = c / 2;
+    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(2, 5, 10));
+}
+
+TEST_CASE("Operator/= with coordinate") {
+    r2d2::robot_arm::coordinate_3D_c c(5, 10, 20);
+    c /= r2d2::robot_arm::coordinate_3D_c(5, 2, 2);
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(1, 5, 10));
+}
+
+TEST_CASE("Operator/= with int") {
+    r2d2::robot_arm::coordinate_3D_c c(10, 100, 200);
+    c /= 2;
+    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(5, 50, 100));
+}
