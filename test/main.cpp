f4bee03ad87e5e89ae293a85fcb69425ba4fe63a
MarcDirven <marc.dirven@student.hu.nl>
2019-04-08 11:56:20 +0200

    renamed class with _c extension

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,4 @@
-#include "vector_3D.hpp"
+#include "vector_3D_c.hpp"
 #include <iostream>
 
 #define CATCH_CONFIG_MAIN
@@ -7,109 +7,109 @@
 #include <iostream>
 
 TEST_CASE("Operator==") {
-    r2d2::robot_arm::vector_3D<float> c(1.f, 5.f, 6.f);
-    r2d2::robot_arm::vector_3D<float> c2(1.f, 5.f, 6.f);
+    r2d2::robot_arm::vector_3D_c<float> c(1.f, 5.f, 6.f);
+    r2d2::robot_arm::vector_3D_c<float> c2(1.f, 5.f, 6.f);
     REQUIRE(c == c2);
 }
 
 TEST_CASE("Default constructor") {
-    r2d2::robot_arm::vector_3D<int> c;
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(0, 0, 0));
+    r2d2::robot_arm::vector_3D_c<int> c;
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(0, 0, 0));
 }
 
 TEST_CASE("Constructor, one value") {
-    r2d2::robot_arm::vector_3D<int> c(5);
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(5, 5, 5));
+    r2d2::robot_arm::vector_3D_c<int> c(5);
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(5, 5, 5));
 }
 
 TEST_CASE("Constructor, three values") {
-    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(1, 2, 3));
+    r2d2::robot_arm::vector_3D_c<int> c(1, 2, 3);
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(1, 2, 3));
 }
 
 TEST_CASE("Operator + with coordinate") {
-    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
-    r2d2::robot_arm::vector_3D<int> result =
-        c + r2d2::robot_arm::vector_3D<int>(1, 2, 3);
-    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(2, 4, 6));
+    r2d2::robot_arm::vector_3D_c<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D_c<int> result =
+        c + r2d2::robot_arm::vector_3D_c<int>(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::vector_3D_c<int>(2, 4, 6));
 }
 
 TEST_CASE("Operator + with int") {
-    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
-    r2d2::robot_arm::vector_3D<int> result = c + 1;
-    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(2, 3, 4));
+    r2d2::robot_arm::vector_3D_c<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D_c<int> result = c + 1;
+    REQUIRE(result == r2d2::robot_arm::vector_3D_c<int>(2, 3, 4));
 }
 
 TEST_CASE("Operator += with coordinate") {
-    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
-    c += r2d2::robot_arm::vector_3D<int>(3, 4, 5);
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(4, 6, 8));
+    r2d2::robot_arm::vector_3D_c<int> c(1, 2, 3);
+    c += r2d2::robot_arm::vector_3D_c<int>(3, 4, 5);
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(4, 6, 8));
 }
 
 TEST_CASE("Operator += with int") {
-    r2d2::robot_arm::vector_3D<int> c(4, 5, 6);
+    r2d2::robot_arm::vector_3D_c<int> c(4, 5, 6);
     c += 3;
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(7, 8, 9));
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(7, 8, 9));
 }
 
 TEST_CASE("Operator++") {
-    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D_c<int> c(1, 2, 3);
     c++;
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(2, 3, 4));
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(2, 3, 4));
 }
 
 TEST_CASE("Operator- with coordinate") {
-    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
-    r2d2::robot_arm::vector_3D<int> result =
-        c - r2d2::robot_arm::vector_3D<int>(1, 2, 3);
-    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(0, 0, 0));
+    r2d2::robot_arm::vector_3D_c<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D_c<int> result =
+        c - r2d2::robot_arm::vector_3D_c<int>(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::vector_3D_c<int>(0, 0, 0));
 }
 
 TEST_CASE("Operator- with int") {
-    r2d2::robot_arm::vector_3D<int> c(5, 2, 3);
-    r2d2::robot_arm::vector_3D<int> result = c - 2;
-    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(3, 0, 1));
+    r2d2::robot_arm::vector_3D_c<int> c(5, 2, 3);
+    r2d2::robot_arm::vector_3D_c<int> result = c - 2;
+    REQUIRE(result == r2d2::robot_arm::vector_3D_c<int>(3, 0, 1));
 }
 
 TEST_CASE("Operator-= with coordinate") {
-    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
-    c -= r2d2::robot_arm::vector_3D<int>(1, 2, 3);
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(4, 2, 0));
+    r2d2::robot_arm::vector_3D_c<int> c(5, 4, 3);
+    c -= r2d2::robot_arm::vector_3D_c<int>(1, 2, 3);
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(4, 2, 0));
 }
 
 TEST_CASE("Operator-= with int") {
-    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
+    r2d2::robot_arm::vector_3D_c<int> c(5, 4, 3);
     c -= 3;
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(2, 1, 0));
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(2, 1, 0));
 }
 
 TEST_CASE("Operator--") {
-    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
+    r2d2::robot_arm::vector_3D_c<int> c(5, 4, 3);
     c--;
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(4, 3, 2));
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(4, 3, 2));
 }
 
 TEST_CASE("Operator/ with coordinate") {
-    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
-    r2d2::robot_arm::vector_3D<int> result =
-        c / r2d2::robot_arm::vector_3D<int>(1, 2, 3);
-    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(5, 2, 1));
+    r2d2::robot_arm::vector_3D_c<int> c(5, 4, 3);
+    r2d2::robot_arm::vector_3D_c<int> result =
+        c / r2d2::robot_arm::vector_3D_c<int>(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::vector_3D_c<int>(5, 2, 1));
 }
 
 TEST_CASE("Operator/ with int") {
-    r2d2::robot_arm::vector_3D<int> c(4, 10, 20);
-    r2d2::robot_arm::vector_3D<int> result = c / 2;
-    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(2, 5, 10));
+    r2d2::robot_arm::vector_3D_c<int> c(4, 10, 20);
+    r2d2::robot_arm::vector_3D_c<int> result = c / 2;
+    REQUIRE(result == r2d2::robot_arm::vector_3D_c<int>(2, 5, 10));
 }
 
 TEST_CASE("Operator/= with coordinate") {
-    r2d2::robot_arm::vector_3D<int> c(5, 10, 20);
-    c /= r2d2::robot_arm::vector_3D<int>(5, 2, 2);
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(1, 5, 10));
+    r2d2::robot_arm::vector_3D_c<int> c(5, 10, 20);
+    c /= r2d2::robot_arm::vector_3D_c<int>(5, 2, 2);
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(1, 5, 10));
 }
 
 TEST_CASE("Operator/= with int") {
-    r2d2::robot_arm::vector_3D<int> c(10, 100, 200);
+    r2d2::robot_arm::vector_3D_c<int> c(10, 100, 200);
     c /= 2;
-    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(5, 50, 100));
+    REQUIRE(c == r2d2::robot_arm::vector_3D_c<int>(5, 50, 100));
 }
