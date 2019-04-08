8d1eb5df1f95ac757fc7afc6b4d87818ec052bb4
MarcDirven <marc.dirven@student.hu.nl>
2019-04-08 11:51:50 +0200

    removed vector3D cpp for template header only library. Renamed the class to vector3D. Passes tests

---
+++ code/headers/vector_3D.hpp
@@ -0,0 +1,233 @@
+#pragma once
+
+/**
+ * Class point_3d is used for xyz 3D coordinates
+ */
+
+namespace r2d2::robot_arm {
+    template<class Type>
+    class vector_3D {
+    public:
+        /**
+         * 3 public member variables x y z.
+         * Public member variables x y z.
+         *
+         * @param x
+         * @param y
+         * @param z
+         **/
+        Type x;
+        Type y;
+        Type z;
+
+        /**
+         * Operator == for equality checking.
+         * Required for catch2.
+         *
+         * @param vector_3D rhs
+         * @return bool
+         **/
+        bool operator==(const vector_3D<Type> &rhs) const {
+            return x == rhs.x && y == rhs.y && z == rhs.z;
+        } 
+
+        /**
+        * vector_3D
+         default constructor.
+        **/
+        vector_3D():
+            x(), y(), z()
+        {}
+
+        /**
+         * vector_3D constructor.
+         * Initializes x, y, z in respectively.
+         *
+         * @param x
+         * @param y
+         * @param z
+         **/
+        vector_3D(const Type &x, const Type &y, const Type &z):
+            x(x), y(y), z(z)
+        {}
+
+        /**
+         * vector_3D constructor.
+         * Initializes x, y, z in with all_values.
+         *
+         * @param int all_values
+         **/
+        vector_3D(const Type &all_values):
+            vector_3D(all_values, all_values, all_values)
+        {}
+
+        /**
+         * vector_3D default destructor.
+         **/
+        ~vector_3D() = default;
+
+        /**
+         * vector_3D operator+.
+         * Sums a point3_d with another vector_3D, returns the result
+         *
+         * @param vector_3D rhs
+         * @return vector_3D
+        
+        **/
+        vector_3D<Type> operator+(const vector_3D<Type> &rhs) const {
+            return vector_3D<Type>(x + rhs.x, y + rhs.y, z + rhs.z);
+        }
+
+        /**
+         * vector_3D operator+.
+         * Increases all values from vector_3D (x, y, z) with the int
+         *parameter rhs
+         *
+         * @param int rhs
+         * @return vector_3D
+         **/
+        vector_3D<Type> operator+(const Type &rhs) const {
+            return *this + vector_3D<Type>(rhs);
+        }
+
+        /**
+         * vector_3D operator +=
+         * increases all values with rhs
+         *
+         * @param vector_3D rhs
+         * @return vector_3D&
+         *
+         **/
+        vector_3D<Type> &operator+=(const vector_3D<Type> &rhs) {
+            *this = *this + rhs;
+            return *this;
+        }
+
+        /** vector_3D operator +=
+         * increases all values with rhs
+         *
+         * @param int rhs
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator+=(const Type &rhs) {
+            return *this += vector_3D(rhs);
+        }   
+
+        /**
+         * vector_3D operator++.
+         * Increases all values from vector_3D (x, y, z) with 1
+         *
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator++(int) {
+            return *this += 1;
+        }
+
+        /**
+         * vector_3D operator-
+         * Decreases all values from point_3d (x, y, z) with rhs
+         *
+         * @param vector_3D
+         * @return vector_3D
+         **/
+        vector_3D<Type> operator-(const vector_3D<Type> &rhs) const {
+            return vector_3D<Type>(x - rhs.x, y - rhs.y, z - rhs.z);
+        }
+
+        /**
+         * vector_3D operator-.
+         * Decreases all values from vector_3D (x, y, z) with the int
+         *parameter rhs
+         *
+         * @param int rhs
+         * @return vector_3D
+         **/
+        vector_3D<Type> operator-(const Type &rhs) const {
+            return *this - vector_3D<Type>(rhs);
+        }
+
+        /**
+         * vector_3D operator -=
+         * Decreases all values with vector_3D rhs
+         *
+         * @param vector_3D rhs
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator-=(const vector_3D<Type> &rhs){
+            *this = *this - rhs;
+            return *this;
+        }
+
+        /**
+         * vector_3D operator -=
+         * Decreases all values with int rhs
+         *
+         * @param vector_3D rhs
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator-=(const Type &rhs) {
+            return *this -= vector_3D<Type>(rhs);
+        }
+
+        /**
+         * vector_3D operator--.
+         * Decreases all values from vector_3D (x, y, z) with 1
+         *
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator--(int) {
+            return *this -= 1;
+        }
+
+        /**
+         * vector_3D operator/
+         * Devides all values with rhs.
+         * Note that this class contains of ints only
+         *
+         * @param vector_3D rhs
+         * @return vector_3D
+         **/
+        vector_3D<Type> operator/(const vector_3D<Type> &rhs) const {
+        return vector_3D<Type>((x / rhs.x),
+                                     (y / rhs.y),
+                                     (z / rhs.z));
+        }
+
+        /**
+         * vector_3D operator/
+         * Devides all values with rhs.
+         * Note that this class contains of ints only
+         *
+         * @param int rhs
+         * @return vector_3D
+         **/
+        vector_3D<Type> operator/(const Type &rhs)const {
+            return *this / vector_3D<Type>(rhs);
+        }
+
+        /**
+         * vector_3D operator/=
+         * Devides all values with rhs.
+         * Note that this class contains of ints only
+         *
+         * @param coordinate3D_c rhs
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator/=(const vector_3D<Type> &rhs) {
+            *this = *this / rhs;
+            return *this;
+        }
+
+        /**
+         * vector_3D operator/=
+         * Devides all values with rhs.
+         * Note that this class contains of ints only
+         *
+         * @param int rhs
+         * @return vector_3D&
+         **/
+        vector_3D<Type> &operator/=(const Type &rhs){
+            return *this /= vector_3D<Type>(rhs);
+        }
+    };
+} // namespace r2d2::robot_arm
--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,4 @@
-#include "coordinate_3D_c.hpp"
+#include "vector_3D.hpp"
 #include <iostream>
 
 #define CATCH_CONFIG_MAIN
@@ -7,109 +7,109 @@
 #include <iostream>
 
 TEST_CASE("Operator==") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 5, 6);
-    r2d2::robot_arm::coordinate_3D_c c2(1, 5, 6);
+    r2d2::robot_arm::vector_3D<float> c(1.f, 5.f, 6.f);
+    r2d2::robot_arm::vector_3D<float> c2(1.f, 5.f, 6.f);
     REQUIRE(c == c2);
 }
 
 TEST_CASE("Default constructor") {
-    r2d2::robot_arm::coordinate_3D_c c;
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(0, 0, 0));
+    r2d2::robot_arm::vector_3D<int> c;
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(0, 0, 0));
 }
 
 TEST_CASE("Constructor, one value") {
-    r2d2::robot_arm::coordinate_3D_c c(5);
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(5, 5, 5));
+    r2d2::robot_arm::vector_3D<int> c(5);
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(5, 5, 5));
 }
 
 TEST_CASE("Constructor, three values") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(1, 2, 3));
+    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(1, 2, 3));
 }
 
 TEST_CASE("Operator + with coordinate") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
-    r2d2::robot_arm::coordinate_3D_c result =
-        c + r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
-    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(2, 4, 6));
+    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D<int> result =
+        c + r2d2::robot_arm::vector_3D<int>(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(2, 4, 6));
 }
 
 TEST_CASE("Operator + with int") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
-    r2d2::robot_arm::coordinate_3D_c result = c + 1;
-    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(2, 3, 4));
+    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D<int> result = c + 1;
+    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(2, 3, 4));
 }
 
 TEST_CASE("Operator += with coordinate") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
-    c += r2d2::robot_arm::coordinate_3D_c(3, 4, 5);
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(4, 6, 8));
+    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
+    c += r2d2::robot_arm::vector_3D<int>(3, 4, 5);
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(4, 6, 8));
 }
 
 TEST_CASE("Operator += with int") {
-    r2d2::robot_arm::coordinate_3D_c c(4, 5, 6);
+    r2d2::robot_arm::vector_3D<int> c(4, 5, 6);
     c += 3;
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(7, 8, 9));
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(7, 8, 9));
 }
 
 TEST_CASE("Operator++") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
+    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
     c++;
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(2, 3, 4));
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(2, 3, 4));
 }
 
 TEST_CASE("Operator- with coordinate") {
-    r2d2::robot_arm::coordinate_3D_c c(1, 2, 3);
-    r2d2::robot_arm::coordinate_3D_c result =
-        c - r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
-    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(0, 0, 0));
+    r2d2::robot_arm::vector_3D<int> c(1, 2, 3);
+    r2d2::robot_arm::vector_3D<int> result =
+        c - r2d2::robot_arm::vector_3D<int>(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(0, 0, 0));
 }
 
 TEST_CASE("Operator- with int") {
-    r2d2::robot_arm::coordinate_3D_c c(5, 2, 3);
-    r2d2::robot_arm::coordinate_3D_c result = c - 2;
-    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(3, 0, 1));
+    r2d2::robot_arm::vector_3D<int> c(5, 2, 3);
+    r2d2::robot_arm::vector_3D<int> result = c - 2;
+    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(3, 0, 1));
 }
 
 TEST_CASE("Operator-= with coordinate") {
-    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
-    c -= r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(4, 2, 0));
+    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
+    c -= r2d2::robot_arm::vector_3D<int>(1, 2, 3);
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(4, 2, 0));
 }
 
 TEST_CASE("Operator-= with int") {
-    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
+    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
     c -= 3;
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(2, 1, 0));
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(2, 1, 0));
 }
 
 TEST_CASE("Operator--") {
-    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
+    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
     c--;
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(4, 3, 2));
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(4, 3, 2));
 }
 
 TEST_CASE("Operator/ with coordinate") {
-    r2d2::robot_arm::coordinate_3D_c c(5, 4, 3);
-    r2d2::robot_arm::coordinate_3D_c result =
-        c / r2d2::robot_arm::coordinate_3D_c(1, 2, 3);
-    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(5, 2, 1));
+    r2d2::robot_arm::vector_3D<int> c(5, 4, 3);
+    r2d2::robot_arm::vector_3D<int> result =
+        c / r2d2::robot_arm::vector_3D<int>(1, 2, 3);
+    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(5, 2, 1));
 }
 
 TEST_CASE("Operator/ with int") {
-    r2d2::robot_arm::coordinate_3D_c c(4, 10, 20);
-    r2d2::robot_arm::coordinate_3D_c result = c / 2;
-    REQUIRE(result == r2d2::robot_arm::coordinate_3D_c(2, 5, 10));
+    r2d2::robot_arm::vector_3D<int> c(4, 10, 20);
+    r2d2::robot_arm::vector_3D<int> result = c / 2;
+    REQUIRE(result == r2d2::robot_arm::vector_3D<int>(2, 5, 10));
 }
 
 TEST_CASE("Operator/= with coordinate") {
-    r2d2::robot_arm::coordinate_3D_c c(5, 10, 20);
-    c /= r2d2::robot_arm::coordinate_3D_c(5, 2, 2);
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(1, 5, 10));
+    r2d2::robot_arm::vector_3D<int> c(5, 10, 20);
+    c /= r2d2::robot_arm::vector_3D<int>(5, 2, 2);
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(1, 5, 10));
 }
 
 TEST_CASE("Operator/= with int") {
-    r2d2::robot_arm::coordinate_3D_c c(10, 100, 200);
+    r2d2::robot_arm::vector_3D<int> c(10, 100, 200);
     c /= 2;
-    REQUIRE(c == r2d2::robot_arm::coordinate_3D_c(5, 50, 100));
+    REQUIRE(c == r2d2::robot_arm::vector_3D<int>(5, 50, 100));
 }
