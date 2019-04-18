865312099d624e767930e0e88f259c825460fe9c
slimmerik <rik.honcoop@student.hu.nl>
2019-04-18 13:55:03 +0200

    clear main

--- code/main.cpp
+++ code/main.cpp
@@ -14,17 +14,4 @@ int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
 
-    vector3i_c v(100,100,100);
-
-    r2d2::uart_ports_c uart_port_one = r2d2::uart_ports_c::uart1;
-
-    auto usart_bus_uarm = r2d2::hardware_usart_c(9600, uart_port_one);
-
-    uarm_swift_pro_c uarm(usart_bus_uarm);
-
-    uarm.init();
-    hwlib::wait_ms(1000);
-    uarm.move_head_to_coordinate(v);
-    hwlib::cout << "done";
-
 }
