356ad6e74edecafa30505057133f3c17c7961c60
ruben <rubenrara@gmail.com>
2019-06-19 14:36:54 +0200

    implemented formules to calculate the angle of the servo`s

--- code/headers/calculate_inverse_kinematics.hpp
+++ code/headers/calculate_inverse_kinematics.hpp
@@ -1,20 +1,22 @@
 #pragma once
 #include <cmath>
-
+#include <hwlib.hpp>
+#include <vector3.hpp>
+#define PI 3.14159265
 namespace r2d2::robot_arm {
 
     class calculate_inverse_kinematics_c {
-    private
+    private:
         uint16_t arm_lengt1, arm_lengt2;
-        uint16_t position_end_effector;
-        uint16_t angle_aplha, angle_beta;
+        vector3i_c position_end_effector;
+        uint16_t angle_aplha, angle_beta, angle_phi, angle_theta;
 
     public:
         /**
-         * Robot arm constructor, needs an usart bus for communication
+         * Robot arm constructor
          */
-        calculate_inverse_kinematics(uint16_t arm_lengt1,
-        uint16_t arm_lengt2));
+        calculate_inverse_kinematics_c(uint16_t arm_lengt1,
+                                       uint16_t arm_lengt2);
         /**
          * @brief
          * This function calculates the angles of the robot arm when a
@@ -32,7 +34,7 @@ namespace r2d2::robot_arm {
          *@return angle of axis alpha
          *
          */
-        uint16_t get_angle_alpha();
+        uint16_t get_angle_theta();
         /**
          * @brief
          * get the angle of beta after it calcualted by the
