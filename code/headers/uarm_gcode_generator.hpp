e1b7e2dc78f76fceb739deee8937ec72808172a4
Dylan van Eck <dylan.v.eck@hotmail.com>
2019-06-26 23:32:36 +0200

    add debug order and add relative displacement to gcode (coordinate/polar)

--- code/headers/uarm_gcode_generator.hpp
+++ code/headers/uarm_gcode_generator.hpp
@@ -4,6 +4,15 @@ namespace r2d2::robot_arm {
     template <size_t Size>
     class uarm_gcode_generator_c : public gcode_generator_c<Size> {
     public:
+
+        void init(const uint8_t &on_off){
+            char on_off_string[2];
+            this->int_to_string(on_off, on_off_string);
+            this->append("#1 M2122 V");
+            this->append(on_off_string);
+            this->append("\n");
+        }
+
         /**
          * Converts a vector3i_c to a gcode command for uArm
          *
@@ -19,7 +28,7 @@ namespace r2d2::robot_arm {
             this->int_to_string(coordinate.y, y_string);
             this->int_to_string(coordinate.z, z_string);
             this->int_to_string(speed, speed_string);
-            this->append("#n G0 X");
+            this->append("#2 G0 X");
             this->append(x_string);
             this->append(" Y");
             this->append(y_string);
@@ -39,7 +48,7 @@ namespace r2d2::robot_arm {
             this->int_to_string(coordinate.y, r_string);
             this->int_to_string(coordinate.z, h_string);
             this->int_to_string(speed, speed_string);
-            this->append("#n G2201 S");
+            this->append("#3 G2201 S");
             this->append(s_string);
             this->append(" R");
             this->append(r_string);
@@ -55,18 +64,50 @@ namespace r2d2::robot_arm {
             char degree_string[4];
             this->int_to_string(id, id_string);
             this->int_to_string(degree, degree_string);
-            this->append("G2202 N");
+            this->append("#4 G2202 N");
             this->append(id_string);
             this->append(" V");
             this->append(degree_string);
             this->append("\n");
         }
 
-        void init(const uint8_t &on_off){
-            char on_off_string[2];
-            this->int_to_string(on_off, on_off_string);
-            this->append("#1 M2122 V");
-            this->append(on_off_string);
+        void relative_displacement_to_gcode(const vector3i_c &coordinate, const uint16_t &speed) {
+            char x_string[11]; // max number of int digits (10) + '\0' = 11
+            char y_string[11];
+            char z_string[11];
+            char speed_string[11];
+            this->int_to_string(coordinate.x, x_string);
+            this->int_to_string(coordinate.y, y_string);
+            this->int_to_string(coordinate.z, z_string);
+            this->int_to_string(speed, speed_string);
+            this->append("#5 G2204 X");
+            this->append(x_string);
+            this->append(" Y");
+            this->append(y_string);
+            this->append(" Z");
+            this->append(z_string);
+            this->append(" F");
+            this->append(speed_string);
+            this->append("\n");
+        }
+
+        void relative_displacement_polar_to_gcode(const vector3i_c &coordinate, const uint16_t &speed){
+            char s_string[11]; // max number of int digits (10) + '\0' = 11
+            char r_string[11];
+            char h_string[11];
+            char speed_string[11];
+            this->int_to_string(coordinate.x, s_string);
+            this->int_to_string(coordinate.y, r_string);
+            this->int_to_string(coordinate.z, h_string);
+            this->int_to_string(speed, speed_string);
+            this->append("#6 G2205 S");
+            this->append(s_string);
+            this->append(" R");
+            this->append(r_string);
+            this->append(" H");
+            this->append(h_string);
+            this->append(" F");
+            this->append(speed_string);
             this->append("\n");
         }
     };
