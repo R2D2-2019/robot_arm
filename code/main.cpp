eb0a1b899640e79128a85d89b522f0681ee2f271
bartbal <bartvannetburg@hotmail.com>
2019-08-10 14:48:09 +0200

    added snam arm loop to main

--- code/main.cpp
+++ code/main.cpp
@@ -2,12 +2,22 @@
 #include <hardware_usart.hpp>
 #include <hwlib.hpp>
 #include <uarm_swift_pro.hpp>
+#include <servo.hpp>
+#include <calculate_inverse_kinematics.hpp>
+#include <3axis_robot_arm.hpp>
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
     auto usart = r2d2::usart::hardware_usart_c<r2d2::usart::usart0>(115200);
     r2d2::robot_arm::uarm_swift_pro_c uarm(usart);
+    auto servo1 = r2d2::robot_arm::servo_c(0); 
+    auto servo2 = r2d2::robot_arm::servo_c(1); 
+    auto servo3 = r2d2::robot_arm::servo_c(2, r2d2::pwm_lib::clocks::CLOCKB); 
+
+    r2d2::robot_arm::calculate_inverse_kinematics_c calculator = r2d2::robot_arm::calculate_inverse_kinematics_c(100, 190); 
+
+    auto snam_arm = r2d2::robot_arm::axis_robot_arm_c(servo1, servo2, servo3, calculator); 
 
     // driver code
     uarm.init();
@@ -16,5 +26,25 @@ int main() {
     // You can add an extra parameter after the vector for speed if you'd like
     // i.e. uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i(100, 100,
     // 100), 500);
+
+    int sleep_time = 2000;
+
+    while (1) { 
+        snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 200, 135)); 
+        hwlib::wait_ms(sleep_time); 
+        snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 40, 135)); 
+        hwlib::wait_ms(sleep_time); 
+        snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 200, 135)); 
+        hwlib::wait_ms(sleep_time); 
+        snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 200, 90)); 
+        hwlib::wait_ms(sleep_time); 
+        snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 40, 90)); 
+        hwlib::wait_ms(sleep_time); 
+        snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 200, 90)); 
+        hwlib::wait_ms(sleep_time); 
+    }
+
+
+
     return 0;
 }
