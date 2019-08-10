75e21b29b02f63e4dd5438aca58cdb167de6d815
bartbal <bartvannetburg@hotmail.com>
2019-08-10 20:42:20 +0200

    implemented move_head_to_Coordinate method with speed overload

--- code/src/snam_arm.cpp
+++ code/src/snam_arm.cpp
@@ -57,11 +57,121 @@ namespace r2d2::robot_arm {
 
     void snam_arm_c::move_head_to_coordinate(const vector3i_c &coordinate,
                                              uint16_t speed) {
-        move_head_to_coordinate(coordinate);
-        // servo's do not have a speed setting. This could be done using steps.
-        // But you would have to calculate the path the arm has to travel.
-        hwlib::cout << "speed is not implemented for the snam arm"
-                    << "\n";
+        //1000 is max speed
+        if(speed > 1000){
+            speed = 1000;
+        }
+
+        calculator.set_position_end_effector(coordinate);
+
+        float theta = calculator.get_angle_theta();
+        float beta = calculator.get_angle_beta();
+        float step = 0;
+        r2d2::robot_arm::angles gamma_angle = servo3.get_last_angle();
+        uint16_t gamma = coordinate.z;
+
+        float beta_rotation = 0;
+        float theta_rotation = 0;
+        float gamma_rotation = 0;
+        bool beta_up = true;
+        bool theta_up = true;
+        bool gamma_up = true;
+
+        r2d2::robot_arm::angles beta_angle = r2d2::robot_arm::angles::beta;
+        r2d2::robot_arm::angles theta_angle = r2d2::robot_arm::angles::theta;
+
+        beta -= 90 - theta;
+
+        if (beta < 0) {
+            beta_angle = r2d2::robot_arm::angles::theta;
+            beta *= -1;
+        }
+        if (theta < 0) {
+            theta_angle = r2d2::robot_arm::angles::beta;
+            theta *= -1;
+        }
+
+        beta = round(beta);
+        theta = round(theta);
+
+        if(gamma < 0){
+        gamma *= -1;
+        gamma_angle = r2d2::robot_arm::angles::beta;
+    } else {
+        gamma_angle = r2d2::robot_arm::angles::theta;
+    }
+
+
+    if(servo1.get_last_degree() < theta){
+        theta_up = true;
+        theta_rotation = theta - servo1.get_last_degree();
+    } else {
+        theta_up = false;
+        theta_rotation = servo1.get_last_degree()- theta;
+    }
+
+    
+    if(servo2.get_last_degree() < beta){
+        beta_up = true;
+        beta_rotation = beta - servo2.get_last_degree();
+    } else {
+        beta_up = false;
+        beta_rotation = servo2.get_last_degree() - beta;
+    }
+
+    if(servo3.get_last_degree() < gamma){
+        gamma_up = true;
+        gamma_rotation = gamma - servo3.get_last_degree();
+    } else {
+        gamma_up = false;
+        gamma_rotation = servo3.get_last_degree() - gamma;
+    }
+    
+    float biggest_rotation = theta_rotation;
+    if(biggest_rotation < beta_rotation){
+        biggest_rotation = beta_rotation;
+    }
+    if(biggest_rotation < gamma_rotation){
+        biggest_rotation = gamma_rotation;
+    }
+    
+    speed = 1000 - speed;
+    //step size per 90 degrees.
+    step = float(90)/float(speed);
+
+
+    while(biggest_rotation > 0){
+        if(theta_rotation > 0){
+            theta_rotation -= step;
+            if(theta_up){
+                servo1.servo_rotate((theta-theta_rotation), r2d2::robot_arm::angles::theta);
+            } else {
+                servo1.servo_rotate((theta_rotation+theta), r2d2::robot_arm::angles::theta);
+            }
+        }
+        if(beta_rotation > 0){
+            beta_rotation -= step;
+            if(beta_up){
+                servo2.servo_rotate((beta-beta_rotation), r2d2::robot_arm::angles::beta);
+            } else {
+                servo2.servo_rotate((beta_rotation+beta), r2d2::robot_arm::angles::beta);
+            }
+        }
+        if(gamma_rotation > 0){
+            gamma_rotation -= step;
+            if(gamma_up){
+                servo3.servo_rotate((gamma-gamma_rotation), gamma_angle);
+            } else {
+                servo3.servo_rotate((gamma_rotation+gamma), gamma_angle);
+            }
+        }
+        biggest_rotation -= step;
+        hwlib::wait_ms(1);
+    }
+
+    servo1.servo_rotate(theta, theta_angle);
+    servo2.servo_rotate(beta, beta_angle);
+    servo3.servo_rotate(gamma, gamma_angle);
     }
 
     void snam_arm_c::rotate_head(int16_t rotation) {
