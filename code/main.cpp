10eb1935370352b0f1a02791ec65d16f528fb197
bartbal <bartvannetburg@hotmail.com>
2019-08-11 12:50:22 +0200

    updated the main, it should now merge better with the master

--- code/main.cpp
+++ code/main.cpp
@@ -9,9 +9,21 @@
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
+
+    //_______________uarm__________________________
     auto usart = r2d2::usart::hardware_usart_c<r2d2::usart::usart0>(115200);
     r2d2::robot_arm::uarm_swift_pro_c uarm(usart);
 
+    // driver code
+    uarm.init();
+    uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(100, 100, 100));
+    uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(25, 175, 100),
+                                 500);
+    // You can add an extra parameter after the vector for speed if you'd like
+    // i.e. uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i(100, 100,
+    // 100), 500);
+
+    //__________________snam_arm_________________________
     auto servo1 = r2d2::robot_arm::servo_c(0); // servo for the first joint.
     auto servo2 = r2d2::robot_arm::servo_c(1); // servo for the second joint.
     auto servo3 = r2d2::robot_arm::servo_c(
@@ -25,15 +37,6 @@ int main() {
 
     auto snam_arm =
         r2d2::robot_arm::snam_arm_c(servo1, servo2, servo3, servo4, calculator);
-
-    // driver code
-    uarm.init();
-    uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(100, 100, 100));
-    uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(25, 175, 100),
-                                 500);
-    // You can add an extra parameter after the vector for speed if you'd like
-    // i.e. uarm.move_head_to_coordinate(r2d2::robot_arm::vector3i(100, 100,
-    // 100), 500);
 
     snam_arm.move_head_to_coordinate(r2d2::robot_arm::vector3i_c(150, 200, 135),
                                      10);
