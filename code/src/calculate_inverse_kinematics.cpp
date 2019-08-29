e37ad3e4ae498cdc7e2c8c26ee6fab664493a1e6
bartbal <bartvannetburg@hotmail.com>
2019-08-29 13:28:04 +0200

    implemented formula to get the angles for the twoo joints

--- code/src/calculate_inverse_kinematics.cpp
+++ code/src/calculate_inverse_kinematics.cpp
@@ -1,36 +1,63 @@
 #include <calculate_inverse_kinematics.hpp>
+#include "hwlib.hpp"
+
 
 namespace r2d2::robot_arm {
+
     calculate_inverse_kinematics_c::calculate_inverse_kinematics_c(
-        uint16_t arm_lengt1, uint16_t arm_lengt2)
-        : arm_length1(arm_lengt1), arm_length2(arm_lengt2) {
-    }
-    void calculate_inverse_kinematics_c::set_position_end_effector(
-        vector3i_c coordinate) {
-
-        int16_t line_a = arm_length2;
-        int16_t line_b = sqrt(pow(coordinate.x, 2.0) + pow(coordinate.y, 2.0));
-        int16_t line_c = arm_length1;
-
-        angle_aplha =
-            acos((pow(line_c, 2.0) - pow(line_b, 2.0) - pow(line_c, 2.0)) /
-                 (-2 * line_c * line_b)) *
-            180 / M_PI;
-        angle_phi = atan2(coordinate.y, coordinate.x) * 180 / M_PI;
-
-        angle_beta =
-            acos(((pow(line_b, 2.0)) - pow(line_c, 2.0) - pow(line_a, 2.0)) /
-                 (-2 * line_a * line_c)) *
-            180 / M_PI;
-
-        angle_theta = angle_aplha + angle_phi;
-    }
-    uint16_t calculate_inverse_kinematics_c::get_angle_theta() {
-        return angle_theta;
+        uint16_t arm_length1, uint16_t arm_length2)
+        : arm_length1(arm_length1), arm_length2(arm_length2) {
     }
 
-    uint16_t calculate_inverse_kinematics_c::get_angle_beta() {
-        return angle_beta;
+    //based on the math from this video: https://www.youtube.com/watch?v=RH3iAmMsolo
+
+    void calculate_inverse_kinematics_c::set_position_end_effector(vector3i_c coordinate) {
+        //setting the x and y and correcting with 80. This has to be changed because negative values are not supported.
+        float x = coordinate.x-80;
+        float y = coordinate.y-80;
+
+        //set arm lengths
+        float l1 = arm_length1;
+        float l2 = arm_length2;
+
+        
+        float temp = pow(x, 2)+pow(y,2)-pow(l1, 2)-pow(l2, 2);
+        temp /= 2*l1*l2;
+        float q2 = acos(temp);
+
+        hwlib::cout << "q2: " << int(q2*10) << "\n";
+
+        temp = l2*sin(q2);
+        temp /= l1 + l2*cos(q2);
+        float q1 = atan(y/x) - atan(temp);
+
+        // float q1 = atan(y/x) - atan( (l2*sin(q2) / (l1 + l2*cos(q2) ) ));
+
+        hwlib::cout << "q1: " << int(q1*10) << "\n";
+
+        // angle_beta = q2*57.2957795;  
+        angle_beta = 180-(q2*57.2957795);  
+
+        if(q1*57.2957795 < 0){
+            angle_theta = (q1*57.2957795) * -1;
+            angle_theta = 180-angle_theta;
+        } else {
+            angle_theta = q1*57.2957795;
+        }
+
+        // angle_theta = sqrt(pow(angle_theta, 2));
+        // angle_beta = sqrt(pow(angle_beta, 2));
+
+        hwlib::cout << "beta(q2): " << angle_beta << " theta(q1): " << angle_theta << "\n";     
     }
 
-} // namespace r2d2::robot_arm
+
+        uint16_t calculate_inverse_kinematics_c::get_angle_theta() {
+            return angle_theta;
+        }
+
+        uint16_t calculate_inverse_kinematics_c::get_angle_beta() {
+            return angle_beta;
+        }
+
+}//end name space r2d2::robot_arm
